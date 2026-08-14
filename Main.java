c8d09a8f8563605cd6df23819f482c296e3510b2
sravanireddypothu <pothusravanireddy2013@gmail.com>
2026-08-14 14:38:30 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI");
+        System.out.println("Continuous Integration is working successfully!");
+    }
+}
